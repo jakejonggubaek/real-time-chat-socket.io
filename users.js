d6fd61cff5821a9aa3e927123a054dc490256dc4
jakejonggubaek <jakejonggubaek@gmail.com>
2021-05-16 22:22:06 -0400

    modified error

--- users.js
+++ users.js
@@ -5,11 +5,11 @@ const addUser = ({id, name, room}) => {
     name = name.trim().toLowerCase();
     room = room.trim().toLowerCase();
 
-    const existingUser = users.find((user) => user.room === room && user.name === name);
+    // const existingUser = users.find((user) => user.room === room && user.name === name);
 
-    if(existingUser) {
-        return { error: 'This username is taken'};
-    }
+    // if(existingUser) {
+    //     return { error: 'This username is taken'};
+    // }
 
     const user = {id, name, room};
     users.push(user);
